52b949066d0113033a5385e83b6ca344213eecc4
maxblandin <maxence.blandin@gmail.com>
2018-04-09 12:07:40 +0200

    Gestion > ConnexionActivity + Fragment

--- app/src/main/java/mbrb/eni/com/locationeuropcar/ecran/ConnexionActivity.java
+++ app/src/main/java/mbrb/eni/com/locationeuropcar/ecran/ConnexionActivity.java
@@ -2,14 +2,32 @@ package mbrb.eni.com.locationeuropcar.ecran;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import mbrb.eni.com.locationeuropcar.R;
+import mbrb.eni.com.locationeuropcar.service.LocationSvc;
 
-public class ConnexionActivity extends AppCompatActivity {
+public class ConnexionActivity extends AppCompatActivity implements ConnexionFragment.OnConnexionListenner {
+
+    LocationSvc locaService= new LocationSvc();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+    }
+
+    @Override
+    public void connexionOK(String identifiant, String mdp) {
+        if(locaService.verifierUtilisateur(identifiant,mdp)){
+            Toast.makeText(this, identifiant + " est connecté ! :)", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Utilisateur inconnu ! :(", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void inscriptionOK(String identifiant, String mdp) {
+
     }
 }
--- app/src/main/java/mbrb/eni/com/locationeuropcar/ecran/ConnexionFragment.java
+++ app/src/main/java/mbrb/eni/com/locationeuropcar/ecran/ConnexionFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import mbrb.eni.com.locationeuropcar.R;
 
@@ -13,18 +15,73 @@ import mbrb.eni.com.locationeuropcar.R;
  * A simple {@link Fragment} subclass.
  */
 public class ConnexionFragment extends Fragment {
+    private  OnConnexionListenner mListenner;
+
+    EditText txtIdentifiant;
+    EditText txtMotDePasse;
+    EditText txtConfirmation;
+    Button btnConnexion;
+    Button btnInscription;
 
 
     public ConnexionFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connexion, container, false);
+        View view = inflater.inflate(R.layout.fragment_connexion, container, false);
+
+        txtIdentifiant = view.findViewById(R.id.identifiant);
+        txtMotDePasse = view.findViewById(R.id.mot_de_passe);
+        txtConfirmation = view.findViewById(R.id.confirmer_mot_de_passe);
+
+        btnConnexion = view.findViewById(R.id.se_connecter);
+        btnInscription = view.findViewById(R.id.inscription);
+
+        if(mListenner !=null){
+            gestionVueConnexion();
+        }
+
+        this.btnConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mListenner !=null){
+                    mListenner.connexionOK(txtIdentifiant.getText().toString(),txtMotDePasse.getText().toString());
+                }
+            }
+        });
+
+        this.btnInscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mListenner != null){
+                    gestionVueInscription();
+                    if(txtMotDePasse.getText().toString().equals(txtConfirmation.getText().toString())){
+                        mListenner.inscriptionOK(txtIdentifiant.getText().toString(),txtMotDePasse.getText().toString());
+                    }
+                }
+            }
+        });
+
+        return view;
     }
 
+    public void gestionVueConnexion(){
+        this.txtConfirmation.setVisibility(View.GONE);
+        this.btnConnexion.setVisibility(View.VISIBLE);
+        this.btnInscription.setText("Inscription");
+    }
+
+    private void gestionVueInscription(){
+        this.txtConfirmation.setVisibility(View.VISIBLE);
+        this.btnConnexion.setVisibility(View.GONE);
+        this.btnInscription.setText("S'inscrire");
+
+    }
+
+    public interface OnConnexionListenner{
+        void connexionOK(String identifiant,String mdp);
+        void inscriptionOK(String identifiant,String mdp);
+    }
 }
